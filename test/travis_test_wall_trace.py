569accbe3a5de8304d66ba20661430910c329376
Masashi Uehara <s1126017jn@s.chibakoudai.jp>
2018-02-06 18:23:15 +0000

    for wall_trace test

--- test/travis_test_wall_trace.py
+++ test/travis_test_wall_trace.py
@@ -4,34 +4,31 @@ import rosnode, rospy
 import time
 
 class WallTraceTest(unittest.TestCase):
-	def set_and_get(self,lf,ls,rs,rf):
-		with open("/dev/rtlightsensor0","w") as f:
-                f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('wall_trace',nodes, "node does not exist")
+    
+    def set_sensor_values(self,lf,ls,rs,rf):
+        with open("/dev/rtlightsensor0","w") as f:
+            f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
 
-        time.sleep(0.3)
-
+    def get_freqs(self):
         with open("/dev/rtmotor_raw_l0","r") as lf,\
              open("/dev/rtmotor_raw_r0","r") as rf:
             left = int(lf.readline().rstrip())
             right = int(rf.readline().rstrip())
-
         return left, right
 
     def test_io(self):
-        left, right = self.set_and_get(400,100,100,0) #total: 600
+        self.set_sensor_values?gc(400,100,100,0) #total: 600
+        time.sleep(0.3)
+        left, right = self.get_freqs()
         self.assertTrue(left == right == 0, "can't stop")
         
-        left, right = self.set_and_get(0,5,1000,0) #side direction is not a trigger of stop 
+        self.set_sensor_values?gc(0,5,1000,0) #side direction is not a trigger of stop 
+        time.sleep(0.3)
+        left, right = self.get_freqs()
         self.assertTrue(left == right != 0, "stop wrongly by side sensors")
-
-        left, right = self.set_and_get(0,10,0,0) #curve to left 
-        self.assertTrue(2000 < left == right, "can't accerelate")
-        
-        left, right = self.set_and_get(0,200,0,0) #curve to right
-        self.assertTrue(2000 < left == right, "can't accerelate")
-
-        left, right = self.set_and_get(0,5,0,0) #don't control when far from a wall
-        self.assertTrue(0 < left == right, "curve wrongly")
 
 if __name__ == '__main__':
     time.sleep(3)
