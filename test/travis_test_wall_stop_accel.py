e10d19e1bd32f5df13cce8cb77da1ede04debc2a
Masashi Uehara <s1126017jn@s.chibakoudai.jp>
2018-02-06 16:55:11 +0000

    for wall_stop test

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -4,32 +4,34 @@ import rosnode, rospy
 import time
 
 class WallStopAccelTest(unittest.TestCase):
-    def set_and_get(self,lf,ls,rs,rf):
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('wall_stop_accel',nodes, "node does not exist")
+    
+    def set_sensor_values(lf,ls,rs,rf):
         with open("/dev/rtlightsensor0","w") as f:
             f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
 
-        time.sleep(0.3)
-
+    def get_freqs(self):
         with open("/dev/rtmotor_raw_l0","r") as lf,\
              open("/dev/rtmotor_raw_r0","r") as rf:
             left = int(lf.readline().rstrip())
             right = int(rf.readline().rstrip())
-
         return left, right
 
     def test_io(self):
-        left, right = self.set_and_get(400,100,100,0) #total: 600
+        left, right = self.set_sensor_values(400,100,100,0) #total: 600
         self.assertTrue(left == right ==0, "can't stop")
         
-        left, right = self.set_and_get(40,0,0,99) #total: 49 
+        left, right = self.set_sensor_values(40,0,0,99) #total: 49 
         self.assertTrue(0 < left == right < 1000, "can't move again")
 
         time.sleep(5.0)
-        left, right = self.set_and_get(40,0,0,99) #total: 49 
+        left, right = self.set_sensor_values(40,0,0,99) #total: 49 
         self.assertTrue(2000 < left == right, "can't accerelate")
 
 
-        left, right = self.set_and_get(15,0,20,15) #total: 50
+        left, right = self.set_sensor_values(15,0,20,15) #total: 50
         self.assertTrue(left == right == 0, "can't stop again")
 
 if __name__ == '__main__':
