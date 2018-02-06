832ca7ba03ab921d9a67e30cd11c524749b6104a
Masashi Uehara <s1126017jn@s.chibakoudai.jp>
2018-02-06 16:39:45 +0000

    for wall_stop test

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -22,13 +22,13 @@ class WallStopTest(unittest.TestCase):
         return left, right
 
     def test_io(self):
-        left, right = self.set_and_get(400,100,100,0) #total: 600
+        left, right = self.set_sensor_values(400,100,100,0) #total: 600
         self.assertTrue(left == 0 and right ==0, "can't stop")
         
-        left, right = self.set_and_get(400,0,0,99) #total: 499 
+        left, right = self.set_sensor_values(400,0,0,99) #total: 499 
         self.assertTrue(left != 0 and right != 0, "can't move again")
 
-        left, right = self.set_and_get(150,0,200,150) #total: 500
+        left, right = self.set_sensor_values(150,0,200,150) #total: 500
         self.assertTrue(left == 0 and right ==0, "can't stop")
 
 if __name__ =='__main__':
